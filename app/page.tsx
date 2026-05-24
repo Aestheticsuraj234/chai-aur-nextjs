bc241cf8003f237a85a55ed1ae43e8c798732c9f
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-24 22:03:16 +0530

    api routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-import { Button } from "@/components/button";
-import Image from "next/image";
+import { TodoApp } from "@/components/todos/todo-app";
+import { fetchTodos } from "@/lib/todos";
 
 export default async function Home() {
-  const res = await fetch("https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10");
-  const data = await res.json();
-  console.log(data);
+  const todos = await fetchTodos();
 
-  // console.log(window)
-  return (
-   <div>
-    <h1 className="text-2xl font-bold">Hello World</h1>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius facere provident, atque rem maxime deserunt natus repellendus eaque tempore fuga numquam aut consequatur magnam quisquam, incidunt, explicabo voluptatibus vero? Ratione fugiat officiis voluptatem iure sapiente ea aut dignissimos doloremque itaque?
-    </p>
-    <Button/>
-   </div>
-  );
+  return <TodoApp initialTodos={todos} />;
 }
